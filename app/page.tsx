91f8ab19469734b6b0fcdea48f865586852edf29
Sean Biava <seanbiava@gmail.com>
2024-08-27 14:46:06 -0700

    Need to figure out this background image, then ready for a placeholder deploy

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,14 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <Header /> 
-        <p>I'm going to test a "card" component here</p>
+        {/* <p>I'm going to test a "card" component here</p>
 
 
         <p>Here is where I'm going to test my timeline component</p>
 
         <p>Here is where I'm going to test my "testimony" component</p>
         <Testimony /> 
-        <Footer /> 
+        <Footer />  */}
       </div>
     </main>
   );
