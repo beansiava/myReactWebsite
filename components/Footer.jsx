95dbda48c6fd8eaff9f660cc52dc0583239a2933
Sean Biava <seanbiava@gmail.com>
2024-08-30 16:10:32 -0700

    Modal is reusable and ready for a "Projects" component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,32 +1,49 @@
 "use client"
 import React, {Component, useState} from 'react';
+import Modal from "./Modal"
 
-class Footer extends Component {
-    state = {  } 
-    render() { 
-        return (
+const Footer = () => {
 
-            <div class="container">
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-              <p class="col-md-10 mb-0 text-body-primary">© 2024 Sean Biava, All Rights Reserved</p>
-          
-              <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                <svg class="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
-              </a>
-            
-              {/* <ul class="nav col-md-4 justify-content-end">
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">Features</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">Pricing</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">About</a></li>
-              </ul> */}
-            </footer>
-          </div>
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => setModalOpen(true);
+  const handleCloseModal = () => setModalOpen(false);
+
+  
+  return ( 
 
 
-        );
-    }
+
+
+      <div class="container">
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p class="col-md-10 mb-0 text-body-primary">© 2024 Sean Biava, All Rights Reserved</p>
+    
+        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+          <svg class="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
+        </a>
+
+        <div>
+
+          <button onClick={handleOpenModal}>Open Modal</button>
+          <Modal isOpen={isModalOpen} onClose={handleCloseModal} title="Project: Card Advantage" children="test">
+            <p>This is a reusable modal component in a function component!</p>
+          </Modal>
+        </div>
+      
+
+        {/* <ul class="nav col-md-4 justify-content-end">
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">Home</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">Features</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">Pricing</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">FAQs</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">About</a></li>
+        </ul> */}
+      </footer>
+    </div>
+
+   );
 }
- 
+
 export default Footer;
+
