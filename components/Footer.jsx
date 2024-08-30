8cd742aee50d13f851bf7833368d24349d36d21a
Sean Biava <seanbiava@gmail.com>
2024-08-30 15:38:45 -0700

    updating

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,18 +1,20 @@
 "use client"
 import React, {Component, useState} from 'react';
+import projectModal from "./projectModal"
 
-class Footer extends Component {
-    state = {  } 
-    render() { 
-        return (
+function Footer() {
+  return ( 
 
-            <div class="container">
+          <div class="container">
             <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
               <p class="col-md-10 mb-0 text-body-primary">© 2024 Sean Biava, All Rights Reserved</p>
           
               <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                 <svg class="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
               </a>
+
+
+
             
               {/* <ul class="nav col-md-4 justify-content-end">
                 <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">Home</a></li>
@@ -25,8 +27,7 @@ class Footer extends Component {
           </div>
 
 
-        );
-    }
+   );
 }
- 
+
 export default Footer;
